fc739d95af41061b212446d6b24fa8b41bd3d377
Martin Popelak <pupca@pupca.cz>
2015-07-19 18:14:10 +0200

    json

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require 'json'
 
 get '/game/:id.json' do
   content_type :json
-  { :response => {:id => '234567865432', :venues => [
+  { :response => {:id => '234567865432', :name => "Pupcas party", :venues => [
   	{:id => 1, :lat => 50.081747, :long => 14.427189, :title => "Vaclavske Namesti", :level => "easy"},
   	{:id => 2, :lat => 48.853094, :long => 16.053431, :title => "Namesti Republiky", :level => "easy"},
   	{:id => 3, :lat => 50.087371, :long => 14.420820, :title => "Staromestske Namesti", :level => "easy"}
