d23d8d2170d5d69a071caba521f9fd7763d6685d
Martin Popelak <pupca@pupca.cz>
2015-07-19 17:49:48 +0200

    json

--- main.rb
+++ main.rb
@@ -3,9 +3,9 @@ require 'json'
 
 get '/game/:id.json' do
   content_type :json
-  { :game_id => '234567865432', :map_objects => [
-  	{:lat => 50.081747, :long => 14.427189, :title => "Vaclavske Namesti", :level => "easy"},
-  	{:lat => 48.853094, :long => 16.053431, :title => "Namesti Republiky", :level => "easy"},
-  	{:lat => 50.087371, :long => 14.420820, :title => "Staromestske Namesti", :level => "easy"}
+  { :id => '234567865432', :venues => [
+  	{:id => 1, :lat => 50.081747, :long => 14.427189, :title => "Vaclavske Namesti", :level => "easy"},
+  	{:id => 2, :lat => 48.853094, :long => 16.053431, :title => "Namesti Republiky", :level => "easy"},
+  	{:id => 3, :lat => 50.087371, :long => 14.420820, :title => "Staromestske Namesti", :level => "easy"}
   ] }.to_json
 end
